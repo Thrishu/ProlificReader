72028c025a3336d75b13532b0a80a55c1ec971e9
Thrishu <91140151+Thrishu@users.noreply.github.com>
2024-04-27 09:27:26 +0530

    first commit

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -35,7 +35,9 @@ function Dashboard({subscriptionPlan}: PageProps) {
   })
   
   return (
-    <main className="mx-auto max-w-7xl md:p-10">
+  <main className="mx-auto max-w-7xl md:p-10 bg-cover bg-center h-screen" style={{backgroundImage: "url('/bk.jpg')"}}>
+ 
+
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
 
